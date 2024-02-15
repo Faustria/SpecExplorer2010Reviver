5798ceba7f920bd9ff9c907860cb38780c2c5887
Kunze <cyphysys@aol.de>
2024-02-15 07:47:01 +0100

    Adding state viewer. adding log viewer.

--- Reviver/Reviver.cs
+++ Reviver/Reviver.cs
@@ -3,10 +3,12 @@
 // *
 // * Description:  
 // *    Used for this tool compiliation are:
-// *    Microsoft.SpecExplorer.Core (Contains Explorartion Manager, Modeling Guidance, Viewers etc.)
-// *    Microsoft.Msagl
-// *    Microsoft.Msagl.Drawing
-// *    Microsoft.Msagl.GraphViewerGDI
+// *    Microsoft.SpecExplorer.Core (explorartion manager, modeling guidance etc.)
+// *    Microsoft.Msagl (transitien viewer)
+// *    Microsoft.Msagl.Drawing (transitien viewer)
+// *    Microsoft.Msagl.GraphViewerGDI (transitien viewer)
+// *    Microsoft.Xrt (state viewer)
+// *    Microsoft.SpecExplorer.ObjectModel (exploration results)
 // *    
 // *    Used for model compilation are:
 // *    Microsoft.Xrt.Runtime
@@ -18,7 +20,27 @@
 // *    All Spec Explorer 2010 Plug-In binaries!
 // *
 // *    ATTENTION: Adapt all absolute paths in main-method.
+// *               Adapt also position of windows and sizes.
 // *
+// *    xrt.config: switches which can only be set in this config file using "D".
+// *    Z3LogFilePath
+// *    EnableDebugInfo
+// *    PauseAtEnd
+// *	SuspendsOnEachStep
+// *	SuspendsOnEnterExit
+// *	PlatformAssembliesLocation
+// *	RegressionMode
+// *	EntryPoint
+// *	MachineName
+// *	Script
+// *	CheckOnly
+// *	ExplorationMode
+// *	DepthFirstSearch
+// *	LogFile
+// *	AppendLog
+// *	IgnoreThreadIdOfRuleControlState
+// *	AllowLoadingRuntimeAssemblies
+// *	DoNotLockFiles
 // ******************************************************************************
 using System;
 using System.Collections.Generic;
@@ -31,192 +53,310 @@ using Microsoft.Msagl.GraphViewerGdi;
 using Microsoft.Msagl.Drawing;
 using System.Windows;
 using System.Diagnostics;
-using Microsoft.SpecExplorer.CommandLine;
+using Microsoft.SpecExplorer.ObjectModel;
+using System.IO;
 
 namespace Reviver
 {
-	// Retrieving message-class for SpecExplorer application.
-    class SpecExplorerHost : Microsoft.SpecExplorer.IHost
-    {
-        public Microsoft.SpecExplorer.MessageResult DecisionDialog(string title, string message, Microsoft.SpecExplorer.MessageButton messageButton)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void DiagMessage(Microsoft.SpecExplorer.DiagnosisKind kind, string message, object location)
-        {
-            Debug.Write(message); ;
-        }
-
-        public IWin32Window DialogOwner
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public Exception FatalError(string message, params Exception[] exceptions)
-        {
-            throw new NotImplementedException();
-        }
-
-        public object GetService(Type type)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Log(string line)
-        {
-        	Debug.Write(line);
-        }
-
-        public bool Logging
-        {
-            get { throw new NotImplementedException(); }
-        }
-
-        public DialogResult ModalDialog(Form form)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void NavigateTo(string fileName, int line, int column)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void NotificationDialog(string title, string message)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void ProgressMessage(Microsoft.SpecExplorer.VerbosityLevel verbosity, string message)
-        {
-        	Debug.WriteLine(message);
-        }
-
-        public EventHandler Protect(EventHandler handler)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void RecoverFromFatalError(Exception exception)
-        {
-            // Even correct execution can end here.
-            throw new NotImplementedException();
-        }
-
-        public void RunProtected(Microsoft.SpecExplorer.ProtectedAction action)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool TryFindLocation(Microsoft.SpecExplorer.MemberInfo member, out Microsoft.SpecExplorer.TextLocation location)
-        {
-            throw new NotImplementedException();
-        }
-
-        public bool TryGetExtensionData(string key, object inputValue, out object outputValue)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Microsoft.SpecExplorer.VerbosityLevel Verbosity
-        {
-            get { throw new NotImplementedException(); }
-        }
-    }
-
     class Program
     {
     	// Spec Explorer binaries path.
-    	public static string SpecExplorer = "";
+    	public const string SpecExplorer = @"D:\Projekte\SpecExplorerBinaries\Specexplorer.exe";
     	
+    	// Spec Explorer exploration result path.
+    	public const string ExplorationResults ="ExplorationResults";
+    	
+    	// Binaries sub folder.
+    	public const string BinFolder = @"\bin\Debug";
+    		
     	// Command list for Spec Explorer executable.
     	public enum Commands { persistexploration, generatetests};
 
+    	// StateViewer Form.
+    	private static Form frmStateViewer;
+    	
+    	// TransitionViewer Form.
+    	private static Form frmTransitionViewer;
+
+    	// LogViewer Form.
+    	private static Form frmLogViewer;
+    	
+    	// Exploration state result control.
+    	private static Microsoft.Xrt.UI.StateBrowserControl sbcStateControl;
+    	
+    	// Transition result control.
+    	private static GViewer gvTransitionControl;
+    	
+    	// The full data of the result.
+    	private static ExplorationResultLoader erlResult;
   	
     	// Start Spec Explorer.
     	// Example:	Reviver (@"D:\SymbolicTest\bin\Debug", "SymbolicTest.dll,Service.dll", "SymbolicTest.cord,Standard.cord","SymbolicFunctionTest");
-    	static void Reviver (string WorkingDirectory, string ModelLibs, string CordScripts, string Machine)
+    	static void Reviver (string WorkingDirectory, List<string>  ModelLibs, List<string> CordScripts, string Machine)
     	{
     		// Command line for shell-executable.
     		string Command =  
     			"/task:" + Enum.GetName(typeof (Commands), Commands.persistexploration) + " " + 
-    			"/assemblies:" + ModelLibs + " "  +
-    			"/scripts:" + CordScripts + " " +
-    			"/machines:" + Machine;
+    			"/assemblies:" + String.Join(",", ModelLibs) + " "  +
+    			"/scripts:" + WorkingDirectory + "\\" + CordScripts.Aggregate((ii, jj) => ii + "," + WorkingDirectory + "\\" + jj) + " " +
+    			"/machines:" + Machine + " " +
+    			"/ExplorationResultPath:" + WorkingDirectory + "\\" + ExplorationResults  + " " +
+    			"/Reexplore" + " " + 
+    			"/verbose";
             
     		// Command line for lib-interface.
     		string[] CommandArray = new []{
                 "/task:" + Enum.GetName(typeof(Commands), Commands.persistexploration) + " " ,
-                "/assemblies:" + ModelLibs + " " ,
-                "/scripts:" + CordScripts + " " ,
-                "/machines:" + Machine};
+                "/assemblies:" + WorkingDirectory + BinFolder + "\\" + ModelLibs.Aggregate((ii, jj) =>   ii + "," + WorkingDirectory + BinFolder + "\\"  + jj),
+                "/scripts:" +  WorkingDirectory + "\\" + CordScripts.Aggregate((ii, jj) => ii + "," + WorkingDirectory + "\\" + jj),
+                "/machines:" + Machine,
+    		    "/ExplorationResultPath:" + WorkingDirectory + "\\" + ExplorationResults,
+                //"/ReplayTestResultFile:" 
+    		    "/Reexplore",
+    		    "/verbose"};
 
     	
     		// Start SpecExplorer using shell.
     		Process p = new Process();
 			p.StartInfo.FileName = SpecExplorer ;
 			p.StartInfo.Arguments = Command;
-			p.StartInfo.WorkingDirectory = WorkingDirectory;
+			p.StartInfo.WorkingDirectory = WorkingDirectory + BinFolder;
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.RedirectStandardOutput = true;
 			p.Start();
  			string output = p.StandardOutput.ReadToEnd(); 			
  			Debug.Write(output);
-
-			/* 			
+			
+			
     	    // Start SpecExplorer using lib-interface.
             // We have to add this to the *.exe.config 
             // <startup useLegacyV2RuntimeActivationPolicy="true">
             //       <supportedRuntime version="v4.0"/>
             // </startup> to run!
-            // All binaries, model etc. must be in build directory!
+            // All binaries, model etc. must be in build directory of Reviver!
+            // or (as we do here, give full paths.
+            /*
     	    SpecExplorerHost Host = new SpecExplorerHost();
             CommandLineParser Parser = new CommandLineParser(Host);
+            
             Parser.ParseCommand(CommandArray);
             ConsoleHostDriver Driver = new ConsoleHostDriver(Host,Parser);
             int i, j;
             Driver.Run(out i, out j);
             */
-
+    	}   
+    	
+    	// Update state data in state view.
+    	static void UpdateStateView (string StateName)
+    	{
+            var erResult = erlResult.LoadExplorationResult();
+            // State data - in a tree node structure.
+            // Shared values is the content.
+            var seData = erResult.SharedEntities;
+            // Just a descriptor.
+            Microsoft.Xrt.UI.StateNodeInfomation sniNode = LoadStateInfo(StateName, erlResult);
+            // Tree data content is assembled from tree structure in the grid view.
+            sbcStateControl.LoadStates(seData,new Microsoft.Xrt.UI.StateNodeInfomation[1]{sniNode});
+    		frmStateViewer.Refresh();
     	}
     	
+    	// Prepare a description for the state information consumed by state viewer.
+    	static Microsoft.Xrt.UI.StateNodeInfomation LoadStateInfo(string state, ExplorationResultLoader loader)
+		{
+    		Microsoft.SpecExplorer.ObjectModel.Node state2 = null;
+  			StateEntity stateEntity = loader.LoadState(state);
+  			state2 = stateEntity.Content;
+    		Dictionary<string, object> dictionary = new Dictionary<string, object>();
+    		dictionary.Add("Description", state);
+    		return new Microsoft.Xrt.UI.StateNodeInfomation(state2, state, dictionary);
+    	}
     	
-    	// Open window with exploration graph result.
-    	static void Viewer (string FileName)
+    	// Open the state view.
+    	static void StateView ()
     	{
+            // Create a form.
+            frmStateViewer = new Form();
+            frmStateViewer.SuspendLayout();
+           
+            sbcStateControl = new Microsoft.Xrt.UI.StateBrowserControl();
+			// Set the column size of first column.
+            //sbcStateControl.Columns[0].Width = 200;
+            UpdateStateView("S0");
+           
+            frmStateViewer.Controls.Add(sbcStateControl);
+            
+            // Show the form. 
+            // Here you can adjust the window position.
+            var Location = new System.Drawing.Point(600,0);
+            frmStateViewer.Location = Location;
+            frmStateViewer.StartPosition = FormStartPosition.Manual;
+            // Here you can adjust the window size.            
+            frmStateViewer.Size = new System.Drawing.Size(424,300);
+            frmStateViewer.ResumeLayout();
+            frmStateViewer.Show();    		
+    		frmStateViewer.Refresh();    	
+    	}
+    	
+		// Open the transition window.    	
+    	static void TransitionView (string Directory, string Machine)
+    	{
+			// Create Filename.
+    		string FileName = Directory + @"\" + ExplorationResults + @"\" + Machine + ".seexpl";    		
             // Create a Spec Explorer view definition.
             ViewDefinition ViewDef = new ViewDefinition();
             // Load the exploration result in seexpl-format.
             Graph ExplorationGraph = ViewDocumentControl.PersistenceFileToGraphForTest(FileName, ViewDef);
             // Create a form.
-            Form form = new Form();
+            frmTransitionViewer = new Form();
             // Create an MSAGL viewer object.
-            GViewer viewer = new GViewer();
+            gvTransitionControl = new GViewer();
             // Bind the graph to the viewer.
-            viewer.Graph = ExplorationGraph;
+            gvTransitionControl.Graph = ExplorationGraph;
             // Associate the viewer with the form.
-            form.SuspendLayout();
-            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
-            form.Controls.Add(viewer);
-            form.ResumeLayout();
-            //Show the form 
-            form.ShowDialog();
+            frmTransitionViewer.SuspendLayout();
+            gvTransitionControl.Dock = System.Windows.Forms.DockStyle.Fill;
+ 
+            frmTransitionViewer.Controls.Add(gvTransitionControl);
+            
+            // Show the form. 
+            // Here you can adjust the window position.
+            var Location = new System.Drawing.Point(0,0);
+            frmTransitionViewer.Location = Location;
+            frmTransitionViewer.StartPosition = FormStartPosition.Manual; 
+            // Here you can adjust the window size.
+            frmTransitionViewer.Size = new System.Drawing.Size(600,600);
+            frmTransitionViewer.ResumeLayout();
+            frmTransitionViewer.Show();  
+            frmTransitionViewer.Refresh();            
     	}
+    	
+    	// Open the exploration result data.
+    	static void LoadResult (string Directory, string Machine)
+    	{
+     		// Create Filename.
+    		string FileName = Directory + @"\" + ExplorationResults + @"\" + Machine + ".seexpl";    		
+
+    		erlResult = new ExplorationResultLoader(FileName);
+    	}
+    	
+    	// Show log file results.
+    	static void ShowLogView (string Directory)
+    	{
+    		// The XRT log file for the z3 commands can be enabeld via xrt.config switch.
+			// <D>
+            //    "Z3LogFilePath=z3.log"
+            // </D>
+    		string sZ3LogFile = Directory + BinFolder + "\\z3.log";
+    		string sContents;
+    		
+    		// Try to load the log file.
+    		try
+    		{
+    			sContents = File.ReadAllText(sZ3LogFile);
+    		}
+    		catch
+    		{
+    			sContents = "";
+    		}
+    		
+    		// Create a form.
+            frmLogViewer = new Form();
+            frmLogViewer.SuspendLayout();
+           
+            TextBox tbLogText = new TextBox();
+            
+            
+            tbLogText.ScrollBars = ScrollBars.Both;
+			tbLogText.WordWrap = false;
+			tbLogText.Size = new System.Drawing.Size(400, 50);
+            //tbLogText.Text = sContents;
+            tbLogText.Multiline = true;
+            tbLogText.AppendText (sContents);
+			frmLogViewer.Controls.Add(tbLogText);
+            
+            
+            // Show the form. 
+            // Here you can adjust the window position.
+            var Location = new System.Drawing.Point(600,300);
+            frmLogViewer.Location = Location;
+            frmLogViewer.StartPosition = FormStartPosition.Manual;
+            // Here you can adjust the window size.            
+            frmLogViewer.Size = new System.Drawing.Size(424,100);
+            frmLogViewer.ResumeLayout();
+            frmLogViewer.Show();    		
+    		frmLogViewer.Refresh();    	
+    	}
+
+    	
+    	// Open all windows with exploration graph, state data, log results.
+    	static void Viewer (string Directory, string Machine)
+    	{    		
+    		ShowLogView(Directory);
+    		LoadResult(Directory, Machine);
+    		StateView();
+    		TransitionView(Directory, Machine);
+    		//var s = frmTransitionViewer.ActiveControl;
+    		var o = gvTransitionControl.SelectedObject;
+    		//gvTransitionControl.DrawingLayoutEditor.SelectedEdge
+    		string Name;
+    		while (true)
+    		{
+    			// Process all user events of the just opened forms.    			
+    			Application.DoEvents();
+    			// Get object in transitient view mouse hovered over. 
+    		    o = gvTransitionControl.SelectedObject;
+    		    try
+    		    {
+    		        Name = o.ToString();
+    		    }
+    		    catch
+    		    {
+    		    	Name ="";
+    		    }
+    		    if (o!= null )
+    		    {
+    		    	if (Name.Length >=  2 & 
+						Name.Substring(0,2) == "\"S")
+    		    	{
+    		    		Name = Name.Split('"')[1];
+    		    		// Try to update according to object under mouse.
+    		    		UpdateStateView(Name);
+    		    	}
+    		    }
+    		}
+    	}    	
+		
+    	// Explore and open window with result.
+		static void Explore (string WorkingDirectory, List<string> ModelLibs, List<string> CordScripts, string Machine)
+    	{
+    		PositionConsoleWindow.PlaceConsoleView();
+    		Reviver (WorkingDirectory, ModelLibs, CordScripts, Machine);
+    		Viewer (WorkingDirectory, Machine);    	
+    	}    	
+    	
+    	// Open window with result.
+    	static void View (string WorkingDirectory, string Machine)
+    	{
+    		PositionConsoleWindow.PlaceConsoleView();
+    		Viewer (WorkingDirectory, Machine);    	
+    	}    	
     	
         static void Main(string[] args)
         {   
-        	// Set path to Spec Explorer Plug-In binaries.
-    		Program.SpecExplorer = @"D:\Projekte\Git_test\SpecExplorer2010Binaries\Specexplorer.exe";
-			
-    		// Run Spec Explorer on a demo model.
-        	Reviver (@"D:\Projekte\Git_test\StaticModel\SpecExplorer1\bin\Debug",
-        	         "SpecExplorer1.dll",
-        	         "Config.cord",
-        	         "DoubleAddScenario");
+
+
+        	// Run Spec Explorer on a demo model.
+        	Explore (@"D:\Projekte\Git_test\StaticModel\SpecExplorer1",
+        	         new List<string> {"SpecExplorer1.dll"},
+        	         new List<string> {"Config.cord"},
+        	         "SlicedAccumulatorModelProgram");
+
         	
-    		// Open viewer on gnerated graph.
-    		Viewer(@"D:\Projekte\Git_test\StaticModel\SpecExplorer1\bin\Debug\ExplorationResults\DoubleAddScenario.seexpl");
+        	
+           // View(@"D:\Projekte\Git_test\StaticModel\SpecExplorer1","SlicedAccumulatorModelProgram");
+            
+            
         }
     }
+
 }
---
+++ Reviver/SpecExplorerHost.cs
@@ -0,0 +1,97 @@
+﻿    
+//using Microsoft.SpecExplorer.CommandLine;
+// Retrieving message-class for SpecExplorer application.
+
+namespace Reviver
+{
+    /*
+	class SpecExplorerHost : Microsoft.SpecExplorer.IHost
+	{
+		public Microsoft.SpecExplorer.MessageResult DecisionDialog(string title, string message, Microsoft.SpecExplorer.MessageButton messageButton)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void DiagMessage(Microsoft.SpecExplorer.DiagnosisKind kind, string message, object location)
+		{
+			Debug.Write(message); 
+			Console.WriteLine(message);
+		}
+
+		public IWin32Window DialogOwner {
+			get { throw new NotImplementedException(); }
+		}
+
+		public Exception FatalError(string message, params Exception[] exceptions)
+		{
+			throw new NotImplementedException();
+		}
+
+		public object GetService(Type type)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Log(string line)
+		{
+			Debug.Write(line);
+			Console.WriteLine(line);
+		}
+
+		public bool Logging {
+			get { throw new NotImplementedException(); }
+		}
+
+		public DialogResult ModalDialog(Form form)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void NavigateTo(string fileName, int line, int column)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void NotificationDialog(string title, string message)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ProgressMessage(Microsoft.SpecExplorer.VerbosityLevel verbosity, string message)
+		{
+			Debug.WriteLine(message);
+			Console.WriteLine(message);
+		}
+
+		public EventHandler Protect(EventHandler handler)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RecoverFromFatalError(Exception exception)
+		{
+			// Even correct execution can end here.
+			throw new NotImplementedException();
+		}
+
+		public void RunProtected(Microsoft.SpecExplorer.ProtectedAction action)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool TryFindLocation(Microsoft.SpecExplorer.MemberInfo member, out Microsoft.SpecExplorer.TextLocation location)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool TryGetExtensionData(string key, object inputValue, out object outputValue)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Microsoft.SpecExplorer.VerbosityLevel Verbosity {
+			get { throw new NotImplementedException(); }
+		}
+	}
+	*/
+}
